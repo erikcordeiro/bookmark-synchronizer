8553ebf1ec92de6a0f820ae93ca49c0d49021b2b
Éric Cordeiro <erikcordeiro@users.noreply.github.com>
2021-10-20 13:16:21 -0300

    Initial commit

---
+++ tests/app.test.ts
@@ -0,0 +1,13 @@
+
+describe('app tests', () => {
+	test('first test', () => {
+		// arrange
+		let result: boolean;
+
+		// act
+		result = true;
+
+		// asserts
+		expect(result).toBeTruthy();
+	})
+})
